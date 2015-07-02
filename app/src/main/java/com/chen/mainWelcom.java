5d168fb8f7e7f74fb83d7185cd2088a57eee96d8
chenYJun <3020739199@qq.com>
2015-07-02 23:18:44 +0800

    欢迎界面完成

---
+++ app/src/main/java/com/chen/mainWelcom.java
@@ -0,0 +1,103 @@
+package com.chen;
+
+/**
+ * Created by yingjun on 2015/6/30.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+
+public class mainWelcom extends Activity {
+
+    //翻页控件
+    private ViewPager mViewPager;
+    private ImageView mPage0;
+    private ImageView mPage1;
+    private ImageView mPage2;
+    private ImageView mPage3;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //去掉标题栏全屏显示
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        mViewPager = (ViewPager)findViewById(R.id.whatsnew_viewpager);
+
+
+        mViewPager.setOnPageChangeListener(new MyOnPageChangeListener());
+        mPage0 = (ImageView)findViewById(R.id.page0);
+        mPage1 = (ImageView)findViewById(R.id.page1);
+        mPage2 = (ImageView)findViewById(R.id.page2);
+        mPage3 = (ImageView)findViewById(R.id.page3);
+
+      /*
+       * 这里是每一页要显示的布局，根据应用需要和特点自由设计显示的内容
+       * 以及需要显示多少页等
+       */
+        LayoutInflater mLi = LayoutInflater.from(this);
+        View view1 = mLi.inflate(R.layout.whats_news_gallery1, null);
+        View view2 = mLi.inflate(R.layout.whats_news_gallery2, null);
+        View view3 = mLi.inflate(R.layout.whats_news_gallery3, null);
+        View view4 = mLi.inflate(R.layout.whats_news_gallery4, null);
+      	/*
+      	 * 这里将每一页显示的view存放到ArrayList集合中
+      	 * 可以在ViewPager适配器中顺序调用展示
+      	 */
+        final ArrayList<View> views = new ArrayList<View>();
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+        views.add(view4);
+
+        //填充ViewPager的数据适配器
+        pagerAdapter mPagerAdapter = new pagerAdapter(views);
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+
+    public class MyOnPageChangeListener implements OnPageChangeListener {
+
+
+        public void onPageSelected(int page) {
+
+            //翻页时当前page,改变当前状态园点图片
+            switch (page) {
+                case 0:
+                    mPage0.setImageDrawable(getResources().getDrawable(R.drawable.page_now));
+                    mPage1.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    break;
+                case 1:
+                    mPage1.setImageDrawable(getResources().getDrawable(R.drawable.page_now));
+                    mPage0.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    mPage2.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    break;
+                case 2:
+                    mPage2.setImageDrawable(getResources().getDrawable(R.drawable.page_now));
+                    mPage1.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    mPage3.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    break;
+                case 3:
+                    mPage3.setImageDrawable(getResources().getDrawable(R.drawable.page_now));
+                    mPage2.setImageDrawable(getResources().getDrawable(R.drawable.page));
+                    break;
+            }
+        }
+
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    }
+}
---
+++ app/src/main/java/com/chen/pagerAdapter.java
@@ -0,0 +1,37 @@
+package com.chen;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class pagerAdapter extends PagerAdapter {
+
+	private ArrayList<View> views;
+	
+	public pagerAdapter(ArrayList<View> views){
+		
+		this.views = views;
+	}
+	@Override
+	public int getCount() {
+		return this.views.size();
+	}
+
+	@Override
+	public boolean isViewFromObject(View arg0, Object arg1) {
+		return arg0 == arg1;
+	}
+	
+	public void destroyItem(View container, int position, Object object) {
+		((ViewPager)container).removeView(views.get(position));
+	}
+	
+	//ҳ��view
+	public Object instantiateItem(View container, int position) {
+		((ViewPager)container).addView(views.get(position));
+		return views.get(position);
+	}
+
+}
